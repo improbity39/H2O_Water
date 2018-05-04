34b75fbceb2ddc755c411751e1390d6d9f3ab602
ASUS <http://mselab.iecs.fcu.edu.tw:50080/D0511383/HW2.git>
2018-05-04 10:42:18 +0800

    add page5 and fix page2's edittext type from name to number

--- app/src/main/java/com/example/user/page2/page2.java
+++ app/src/main/java/com/example/user/page2/page2.java
@@ -3,7 +3,7 @@ package com.example.user.page2;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class page2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
