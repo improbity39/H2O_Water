9d2fe1d31a7da70c291b99759cdd33e719c39503
zaq12392 <macay12392@gmail.com>
2018-06-02 17:00:11 +0800

    finish page3

--- app/src/main/java/com/example/user/page2/page3.java
+++ app/src/main/java/com/example/user/page2/page3.java
@@ -1,13 +1,42 @@
 package com.example.user.page2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class page3 extends AppCompatActivity {
-
+    protected TextView edt_bmi;
+    protected Button exit;
+    protected Button next;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page3);
+        edt_bmi = (TextView)findViewById(R.id.edt_bmi);
+        exit = (Button)findViewById(R.id.bt_exit);
+        next = (Button)findViewById(R.id.bt_next);
+        Intent intent = getIntent();
+        String bmi = intent.getStringExtra("BMI_EXTRA");
+        String age = intent.getStringExtra("AGE_EXTRA");
+        edt_bmi.setText(bmi);
+        exit.setOnClickListener(new Button.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                moveTaskToBack(true);
+                android.os.Process.killProcess(android.os.Process.myPid());
+            }
+        });
+        next.setOnClickListener(new Button.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(page3.this,page4.class);
+                startActivity(intent);
+            }
+        });
     }
 }
