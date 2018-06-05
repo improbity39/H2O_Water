2af8f10da762c7e532937c583a9823c08a73d6cb
improbity39 <improbity39@gmail.com>
2018-06-05 16:10:37 +0800

    change page3 image to circle that can run

--- app/src/main/java/com/example/user/page2/page3.java
+++ app/src/main/java/com/example/user/page2/page3.java
@@ -1,26 +1,61 @@
 package com.example.user.page2;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import java.text.DecimalFormat;
 
 public class page3 extends AppCompatActivity {
     protected TextView edt_bmi;
     protected Button exit;
     protected Button next;
+    private TextView txtProgress;
+    private ProgressBar progressBar;
+    private float pStatus = 0;
+    private Handler handler = new Handler();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page3);
-        edt_bmi = (TextView)findViewById(R.id.edt_bmi);
-        exit = (Button)findViewById(R.id.bt_exit);
-        next = (Button)findViewById(R.id.bt_next);
         Intent intent = getIntent();
         String bmi = intent.getStringExtra("BMI_EXTRA");
         String age = intent.getStringExtra("AGE_EXTRA");
+        DecimalFormat df1 = new DecimalFormat("#.00");
+        txtProgress = (TextView) findViewById(R.id.txtProgress);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        final Double a = Double.parseDouble(bmi);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (pStatus <= (2.5)*a) {
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            progressBar.setProgress((int) pStatus);
+                            double bmi=((float)pStatus);
+                            DecimalFormat df = new DecimalFormat("#.00");
+                            String bmi2 = df.format(bmi);
+                            txtProgress.setText("BMI : " + df.format(Float.parseFloat(bmi2)*0.4));
+                        }
+                    });
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    pStatus+=0.1;
+                }
+            }
+        }).start();
+        edt_bmi = (TextView)findViewById(R.id.edt_bmi);
+        exit = (Button)findViewById(R.id.bt_exit);
+        next = (Button)findViewById(R.id.bt_next);
         edt_bmi.setText(bmi);
         exit.setOnClickListener(new Button.OnClickListener(){
 
