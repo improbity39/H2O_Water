de5d985f82c13703d2d9101e37137aebedada24a
Lee <>
2018-06-15 21:48:19 +0800

    page4

--- app/src/main/java/com/example/user/page2/page4.java
+++ app/src/main/java/com/example/user/page2/page4.java
@@ -3,7 +3,9 @@ package com.example.user.page2;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.ListActivity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.database.DataSetObserver;
 import android.os.CountDownTimer;
 import android.os.Handler;
@@ -25,8 +27,11 @@ import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Collections;
 import java.util.List;
 import java.util.Random;
+
+import static com.example.user.page2.page1.APP_DATA;
 
 
 public class page4 extends Activity {
@@ -38,65 +43,115 @@ public class page4 extends Activity {
     private TextView tx;
     private TextView bm;
     private static final int msgKey1 = 1;
+    protected Boolean first_p4;
     @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page4);
+        SharedPreferences pref = getApplication().getSharedPreferences(APP_DATA, Context.MODE_PRIVATE);
+        final SharedPreferences.Editor editor = pref.edit();
+        first_p4 = pref.getBoolean("KEY_FIRST_p4", true);
         Intent intent = getIntent();
-        buton = (Button)findViewById(R.id.btn_finish);
+        buton = (Button) findViewById(R.id.btn_finish);
         listview = (ListView) findViewById(R.id.lt);
-        tx = (TextView)findViewById(R.id.txv);
-        bm = (TextView)findViewById(R.id.tx_bmi);
+        tx = (TextView) findViewById(R.id.txv);
+        bm = (TextView) findViewById(R.id.tx_bmi);
         String bmi = intent.getStringExtra("BMI");
-        bm.setText("BMI:"+bmi);
+        listShow = new ArrayList<Boolean>();
+        list = new ArrayList<String>();
+        if (first_p4 == true) {
+            editor.putBoolean("KEY_FIRST_p4", false);
+            bm.setText("BMI:" + bmi);
+            editor.putString("BMI", bmi);
+            Random rand = new Random();
+            int num = rand.nextInt(4) + 3;
+            editor.putInt("rand",num);
+            int[] index = new int[num];
+            for (int i = 0; i < num; i++) {
+                index[i] = i;
+            }
+            int count = 0, temp, change1, change2;
+            while (count != 20) {
+                change2 = rand.nextInt(num);
+                change1 = rand.nextInt(num);
+                temp = index[change1];
+                index[change1] = index[change2];
+                index[change2] = temp;
+                count++;
+            }
+            String[] names = {""};
+            for (int x = 0; x < num; x++) {
+                list.add("專案" + (x+1));
+                listShow.add(false);
+                editor.putString("list"+x,list.get(x));
+                editor.putBoolean("listshow"+x,listShow.get(x));
+            }
+            editor.apply();
+
+        } else {
+            bmi = pref.getString("BMI", null);
+            int num_list = pref.getInt("rand",0);
+            bm.setText("BMI:" + bmi);
+            for(int i = 0;i<num_list;i++){
+                list.add(pref.getString("list"+i,null));
+                listShow.add(pref.getBoolean("listshow"+i,false));
+            }
+
+        }
+        ListAdapter adapterItem = new Adapter(this, list);
+        listview.setAdapter(adapterItem);
         Calendar cal = Calendar.getInstance();
-        int dayOfWeek = cal.get(Calendar.DAY_OF_WEEK)-1;
+        int dayOfWeek = cal.get(Calendar.DAY_OF_WEEK) - 1;
         int hour = cal.get(Calendar.HOUR_OF_DAY);
         int minute = cal.get(Calendar.MINUTE);
 
         int day;
-        if(dayOfWeek==0){
-            day=0;
+        if (dayOfWeek == 0) {
+            day = 0;
+        } else {
+            day = 7 - dayOfWeek;
         }
-        else
-        {
-            day = 7-dayOfWeek;
-        }
-        tx.setText("本週結束還剩:"+day+"天"+(24-hour)+"小時"+(60-minute)+"分");
+        tx.setText("本週結束還剩:" + day + "天" + (24 - hour) + "小時" + (60 - minute) + "分");
         Thread t = new Thread(runnable);
         t.start();
         listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                    public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
-                        CheckedTextView chkItem = (CheckedTextView) v.findViewById(R.id.checklist);
-                        chkItem.setChecked(!chkItem.isChecked());
-                        listShow.set(position, chkItem.isChecked());
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                CheckedTextView chkItem = (CheckedTextView) v.findViewById(R.id.checklist);
+                chkItem.setChecked(!chkItem.isChecked());
+                listShow.set(position, chkItem.isChecked());
+                editor.putBoolean("listshow"+position,chkItem.isChecked());
             }
         });
-        listShow = new ArrayList<Boolean>();
-        list = new ArrayList<String>();
-        Random rand = new Random();
-        int num = rand.nextInt(4)+3;
-        int[] index = new int[num];
-        for (int i = 0;i<num;i++){
-            index[i] = i;
+        /*if(first_p4 == true) {
+            listShow = new ArrayList<Boolean>();
+            list = new ArrayList<String>();
+            Random rand = new Random();
+            int num = rand.nextInt(4) + 3;
+            int[] index = new int[num];
+            for (int i = 0; i < num; i++) {
+                index[i] = i;
+            }
+            int count = 0, temp, change1, change2;
+            while (count != 20) {
+                change2 = rand.nextInt(num);
+                change1 = rand.nextInt(num);
+                temp = index[change1];
+                index[change1] = index[change2];
+                index[change2] = temp;
+                count++;
+            }
+            String[] names = {""};
+            for (int x = 1; x <= num; x++) {
+                list.add("專案" + x);
+                listShow.add(false);
+            }
+            ListAdapter adapterItem = new Adapter(this, list);
+            listview.setAdapter(adapterItem);
         }
-        int count = 0,temp,change1,change2;
-        while (count!=20){
-            change2 = rand.nextInt(num);
-            change1 = rand.nextInt(num);
-            temp = index[change1];
-            index[change1] = index[change2];
-            index[change2] = temp;
-            count++;
-        }
-        String[] names = {""};
-        for(int x=1;x<=num;x++) {
-            list.add("專案" + x);
-            listShow.add(false);
-        }
-        ListAdapter adapterItem = new Adapter(this,list);
-        listview.setAdapter(adapterItem);
+        else{
+
+        }*/
         buton.setOnClickListener(new Button.OnClickListener(){
             @Override
             public void onClick(View view) {
